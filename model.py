e42d7812d893b22de566377f52ee7b6719650fe8
onebottlekick <gksqudcks97@gmail.com>
2022-01-07 13:17:41 +0900

    added train function

--- model.py
+++ model.py
@@ -2,6 +2,30 @@ import torch
 from torch import nn
 
 
+def train(model, train_data, val_data, **kwargs):
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=kwargs['learning_rate'])
+    
+    for epoch in range(1, kwargs['epochs']+1):
+        model.train()
+        data, target = train_data
+        outputs = model(data)
+        optimizer.zero_grad()
+        loss = criterion(outputs, target)
+        loss.backward()
+        optimizer.step()
+        print(f'Epoch: {epoch:3d}, train_loss={loss.item():.4f},', end=' ')
+        
+        
+        model.eval()
+        with torch.no_grad():
+            data, target = val_data
+            outputs = model(data)
+            val_loss = criterion(outputs, target).item()
+        print(f'val_loss={val_loss:.4f}')
+
+
+
 class LSTM(nn.Module):
     def __init__(self, input_dim, hidden_dim, num_layers, output_dim):
         super(LSTM, self).__init__()
