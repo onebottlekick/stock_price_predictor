eb40d659a46384a9c5e16f6df471e9e181cda624
onebottlekick <gksqudcks97@gmail.com>
2022-01-07 11:03:07 +0900

    added windowing function

--- utils/data.py
+++ utils/data.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 from typing import Tuple
 import pandas_datareader as pdr
@@ -23,4 +24,24 @@ def load_data(company: str, start:Tuple[int, int, int]=None, end:Tuple[int, int,
     data = pdr.DataReader(company, 'yahoo', start, end)
     return data
 
-data = load_data('AAPL', (2012, 1, 1), (2021, 1, 1))
+
+def windowing(data:pd.DataFrame, window_size:int) -> np.ndarray:
+    '''
+    makes time series data, label
+    
+    Args:
+        data: stock price data
+        window_size: interval date between data and label
+    Return:
+        X, y: data and label
+    Example:
+        >>> X, y = windowing(data, 14)
+    '''
+    X = []
+    y = []
+    
+    for i in range(len(data)-window_size*2):
+        X.append(data[i:i+window_size])
+        y.append(data[i+window_size:i + window_size*2])
+        
+    return np.array(X), np.array(y)
