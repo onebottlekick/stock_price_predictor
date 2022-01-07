b2cf577c84c3e9686892a4616f6bee631fdd55e5
onebottlekick <gksqudcks97@gmail.com>
2022-01-07 11:58:02 +0900

    models
    - LSTM
    - GRU

---
+++ model.py
@@ -0,0 +1,39 @@
+import torch
+from torch import nn
+
+
+class LSTM(nn.Module):
+    def __init__(self, input_dim, hidden_dim, num_layers, output_dim):
+        super(LSTM, self).__init__()
+        
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+        
+        self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, output_dim)
+        
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim, requires_grad=True)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim, requires_grad=True)
+        out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
+        out = self.fc(out)
+        return out
+    
+    
+class GRU(nn.Module):
+    def __init__(self, input_dim, hidden_dim, num_layers, output_dim):
+        super(GRU, self).__init__()
+        
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+        
+        self.gru = nn.GRU(input_dim, hidden_dim, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim, requires_grad=True)
+        out, (hn) = self.gru(x, (h0.detach()))
+        out = self.fc(out) 
+        return out
+    
+
